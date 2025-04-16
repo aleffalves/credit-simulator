ef629e3459910b28fae644d062e20c8d37a302f8
aleff-alves <aleff.alves@tangerino.com.br>
2025-04-16 13:56:43 -0300

    second commit

---
+++ src/main/java/com/github/aleffalves/credit_simulator/CreditSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.aleffalves.credit_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/aleffalves/credit_simulator/CreditSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aleffalves.credit_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
